2a165d0612ffcef479bc229d3bd41945121b5a97
Javier Leonor <javier.leonor1@gmail.com>
2026-02-26 00:50:28 -0600

    feat: Agregado rutas CRUD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import VehicleListPage from './pages/vehicles/VehicleListPage';
+import VehicleformPage from './pages/vehicles/VehicleFormPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <browserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/vehicles" replace />} />
+        <Route path="/vehicles" element={<VehicleListPage />} />
+        <Route path="/vehicles/new" element={<VehicleformPage mode="create" />} />
+        <Route path="/vehicles/:id/edit" element={<VehicleformPage />} />
+      </Routes>
   );
 }
 
